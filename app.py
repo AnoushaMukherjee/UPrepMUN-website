372c5e29694bb37775861cb50a16b716e3171cd8
AnoushaMukherjee <a.mukherjee@universityprep.org>
2024-03-14 09:50:45 -0700

    New

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ socketio = SocketIO(app)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/registration')
 def registration():
