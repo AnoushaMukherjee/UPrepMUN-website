91bc807a0f580fe43d59fc3df00b91fb1817d701
AnoushaMukherjee <a.mukherjee@universityprep.org>
2024-03-14 09:47:29 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# imports
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+# setups and global variables
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# app routes
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/committees')
+def resources():
+    return render_template('committees.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# CALLING EVERYTHING TOGETHER
+if __name__ == '__main__':
+    socketio.run(app)
